79550de095483e34e74d48f4bc48d12afe675e53
Fortes <fortes@MacBook-Air-de-Fortes.local>
2025-10-30 15:51:18 +0100

    ajout de la config

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvf9r0fns73cjb92g-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_cstk_user",
+    "password" : "3p3eL3aVvOPMjTXzXJlL4OZoLOePQxWG",
+    "bdname" :"pollution_cstk" 
     }
 }
 
